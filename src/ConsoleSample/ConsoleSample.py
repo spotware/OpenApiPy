e9c93090977e8408a10bfede6bf41188dacd2007
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-12-07 13:14:08 +0300

    Added new request commands to console sample

--- src/ConsoleSample/ConsoleSample.py
+++ src/ConsoleSample/ConsoleSample.py
@@ -14,6 +14,7 @@ from inputimeout import inputimeout, TimeoutOccurred
 import webbrowser
 
 if __name__ == "__main__":
+    currentAccountId = None
     liveHost = "live.ctraderapi.com"
     demoHost = "demo.ctraderapi.com"
     port = 5035
@@ -57,12 +58,13 @@ if __name__ == "__main__":
         print("\nDisconnected: ", reason)
     
     def onMessageReceived(message): # Callback for receiving all messages
-        if message.payloadType == ProtoHeartbeatEvent().payloadType:
+        if message.payloadType in [ProtoOASubscribeSpotsRes().payloadType, ProtoOAAccountLogoutRes().payloadType, ProtoHeartbeatEvent().payloadType]:
             return
         elif message.payloadType == ProtoOAApplicationAuthRes().payloadType:
             print("API Application authorized")
-        elif message.payloadType == ProtoOAGetAccountListByAccessTokenRes().payloadType:
-            print("Accounts list received: \n", Protobuf.extract(message))
+            if currentAccountId is not None:
+                sendProtoOAAccountAuthReq()
+                return
         elif message.payloadType == ProtoOAAccountAuthRes().payloadType:
             protoOAAccountAuthRes = Protobuf.extract(message)
             print(f"Account {protoOAAccountAuthRes.ctidTraderAccountId} has been authorized")
@@ -71,14 +73,28 @@ if __name__ == "__main__":
         reactor.callLater(3, callable=executeUserCommand)
     
     def onError(failure): # Call back for errors
-        print("Error: ", failure)
+        print("Message Error: ", failure)
 
     def showHelp():
-        print("Commands (Parameters with an * are required)")
+        print("Commands (Parameters with an * are required), ignore the description inside ()")
+        print("setAccount(For all subsequent requests this account will be used) *accountId")
         print("ProtoOAVersionReq clientMsgId")
         print("ProtoOAGetAccountListByAccessTokenReq clientMsgId")
-        print("ProtoOAAccountAuthReq *accountId clientMsgId")
+        print("ProtoOAAssetListReq clientMsgId")
+        print("ProtoOAAssetClassListReq clientMsgId")
+        print("ProtoOASymbolCategoryListReq clientMsgId")
+        print("ProtoOASymbolsListReq includeArchivedSymbols(True/False) clientMsgId")
+        print("ProtoOATraderReq clientMsgId")
+        print("ProtoOASubscribeSpotsReq *symbolId *timeInSeconds(Unsubscribes after this time) subscribeToSpotTimestamp(True/False) clientMsgId")
+        print("ProtoOAReconcileReq clientMsgId")
         reactor.callLater(3, callable=executeUserCommand)
+
+    def setAccount(accountId):
+        global currentAccountId
+        if currentAccountId is not None:
+            sendProtoOAAccountLogoutReq()
+        currentAccountId = int(accountId)
+        sendProtoOAAccountAuthReq()
 
     def sendProtoOAVersionReq(clientMsgId = None):
         request = ProtoOAVersionReq()
@@ -91,46 +107,114 @@ if __name__ == "__main__":
         deferred = client.send(request, clientMsgId = clientMsgId)
         deferred.addErrback(onError)
 
-    def sendProtoOAAccountAuthReq(accountId, clientMsgId = None):
+    def sendProtoOAAccountLogoutReq(clientMsgId = None):
+        request = ProtoOAAccountLogoutReq()
+        request.ctidTraderAccountId = currentAccountId
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+
+    def sendProtoOAAccountAuthReq(clientMsgId = None):
         request = ProtoOAAccountAuthReq()
-        request.ctidTraderAccountId = int(accountId)
+        request.ctidTraderAccountId = currentAccountId
         request.accessToken = accessToken
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+
+    def sendProtoOAAssetListReq(clientMsgId = None):
+        request = ProtoOAAssetListReq()
+        request.ctidTraderAccountId = currentAccountId
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+
+    def sendProtoOAAssetClassListReq(clientMsgId = None):
+        request = ProtoOAAssetClassListReq()
+        request.ctidTraderAccountId = currentAccountId
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+
+    def sendProtoOASymbolCategoryListReq(clientMsgId = None):
+        request = ProtoOASymbolCategoryListReq()
+        request.ctidTraderAccountId = currentAccountId
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+
+    def sendProtoOASymbolsListReq(includeArchivedSymbols = False, clientMsgId = None):
+        request = ProtoOASymbolsListReq()
+        request.ctidTraderAccountId = currentAccountId
+        request.includeArchivedSymbols = includeArchivedSymbols if type(includeArchivedSymbols) is bool else bool(includeArchivedSymbols)
+        deferred = client.send(request)
+        deferred.addErrback(onError)
+
+    def sendProtoOATraderReq(clientMsgId = None):
+        request = ProtoOATraderReq()
+        request.ctidTraderAccountId = currentAccountId
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+
+    def sendProtoOAUnsubscribeSpotsReq(symbolId, clientMsgId = None):
+        request = ProtoOAUnsubscribeSpotsReq()
+        request.ctidTraderAccountId = currentAccountId
+        request.symbolId.append(int(symbolId))
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+
+    def sendProtoOASubscribeSpotsReq(symbolId, timeInSeconds, subscribeToSpotTimestamp	= False, clientMsgId = None):
+        request = ProtoOASubscribeSpotsReq()
+        request.ctidTraderAccountId = currentAccountId
+        request.symbolId.append(int(symbolId))
+        request.subscribeToSpotTimestamp = subscribeToSpotTimestamp if type(subscribeToSpotTimestamp) is bool else bool(subscribeToSpotTimestamp)
+        deferred = client.send(request, clientMsgId = clientMsgId)
+        deferred.addErrback(onError)
+        reactor.callLater(int(timeInSeconds), sendProtoOAUnsubscribeSpotsReq, symbolId)
+
+    def sendProtoOAReconcileReq(clientMsgId = None):
+        request = ProtoOAReconcileReq()
+        request.ctidTraderAccountId = currentAccountId
         deferred = client.send(request, clientMsgId = clientMsgId)
         deferred.addErrback(onError)
 
     commands = {
         "help": showHelp,
+        "setAccount": setAccount,
         "ProtoOAVersionReq": sendProtoOAVersionReq,
         "ProtoOAGetAccountListByAccessTokenReq": sendProtoOAGetAccountListByAccessTokenReq,
-        "ProtoOAAccountAuthReq": sendProtoOAAccountAuthReq}
+        "ProtoOAAssetListReq": sendProtoOAAssetListReq,
+        "ProtoOAAssetClassListReq": sendProtoOAAssetClassListReq,
+        "ProtoOASymbolCategoryListReq": sendProtoOASymbolCategoryListReq,
+        "ProtoOASymbolsListReq": sendProtoOASymbolsListReq,
+        "ProtoOATraderReq": sendProtoOATraderReq,
+        "ProtoOASubscribeSpotsReq": sendProtoOASubscribeSpotsReq,
+        "ProtoOAReconcileReq": sendProtoOAReconcileReq}
 
     def executeUserCommand():
         try:
-            userInput = inputimeout("Command (ex help): ", timeout=15)
+            print("\n")
+            userInput = inputimeout("Command (ex help): ", timeout=17)
         except TimeoutOccurred:
-            reactor.callLater(5, callable=executeUserCommand)
+            print("Command Input Timeout")
+            reactor.callLater(3, callable=executeUserCommand)
             return
         userInputSplit = userInput.split(" ")
         if not userInputSplit:
-            reactor.callLater(5, callable=executeUserCommand)
+            print("Command split error: ", userInput)
+            reactor.callLater(3, callable=executeUserCommand)
             return
         command = userInputSplit[0]
         try:
             parameters = [parameter if parameter[0] != "*" else parameter[1:] for parameter in userInputSplit[1:]]
         except:
             print("Invalid parameters: ", userInput)
-            reactor.callLater(5, callable=executeUserCommand)
+            reactor.callLater(3, callable=executeUserCommand)
         if command in commands:
             commands[command](*parameters)
         else:
             print("Invalid Command: ", userInput)
-            reactor.callLater(5, callable=executeUserCommand)
+            reactor.callLater(3, callable=executeUserCommand)
 
     # Setting optional client callbacks
     client.setConnectedCallback(connected)
     client.setDisconnectedCallback(disconnected)
     client.setMessageReceivedCallback(onMessageReceived)
-    # Set blocking to false if you don't want to block
-    # client will use another thread to run its event loop when blocking is set to false
-    client.startService() # optional timeout in seconds
+    # Starting the client service
+    client.startService()
     reactor.run()
