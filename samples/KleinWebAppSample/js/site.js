4a9d51ae1cbd7ea173812742e2a8444eb9e88401
Ahmad Noman Musleh <afhackermubasher@gmail.com>
2021-12-30 13:38:45 +0300

    Updated web app

--- samples/KleinWebAppSample/js/site.js
+++ samples/KleinWebAppSample/js/site.js
@@ -1,33 +1,16 @@
-﻿function isNumberInRange(object) {
-    var value = parseFloat(object.value);
-    var step = parseFloat(object.step);
+$(document).ready(function () {
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    const token = urlParams.get("token");
 
-    value = value - floatSafeRemainder(value, step);
-
-    object.value = value;
-
-    if (value > parseFloat(object.max))
-        object.value = object.max;
-    else if (value < parseFloat(object.min))
-        object.value = object.min;
-}
-
-function floatSafeRemainder(val, step) {
-    var valDecCount = (val.toString().split('.')[1] || '').length;
-    var stepDecCount = (step.toString().split('.')[1] || '').length;
-    var decCount = valDecCount > stepDecCount ? valDecCount : stepDecCount;
-    var valInt = parseInt(val.toFixed(decCount).replace('.', ''));
-    var stepInt = parseInt(step.toFixed(decCount).replace('.', ''));
-    return (valInt % stepInt) / Math.pow(10, decCount);
-}
-
-function isNumeric(evt) {
-    var theEvent = evt || window.event;
-    var key = theEvent.keyCode || theEvent.which;
-    key = String.fromCharCode(key);
-    var regex = /[0-9]|\./;
-    if (!regex.test(key)) {
-        theEvent.returnValue = false;
-        if (theEvent.preventDefault) theEvent.preventDefault();
-    }
-}
+    $("#sendButton").click(function () {
+        $.getJSON(`/get-data?token=${token}&command=${$('#commandInput').val()}`, function (data, status, xhr) {
+            if ($("#outputTextarea").val() == "") {
+                $("#outputTextarea").val(data["result"]).change()
+            }
+            else {
+                $("#outputTextarea").val($("#outputTextarea").val() + "\n" + data["result"]).change()
+            }
+        });
+    });
+});
--- samples/KleinWebAppSample/main.py
+++ samples/KleinWebAppSample/main.py
@@ -18,15 +18,18 @@ from twisted.internet import endpoints, reactor
 from twisted.web.server import Site
 import sys
 from twisted.python import log
+from twisted.web.static import File
+from twisted.web import resource
+import datetime
 
 host = "localhost"
 port = 8080
 
 credentialsFile = open("credentials-dev.json")
 credentials = json.load(credentialsFile)
+
 auth = Auth(credentials["ClientId"], credentials["Secret"], f"http://{host}:{port}/redirect")
 authUri = auth.getAuthUri()
-
 app = Klein()
 
 @app.route('/')
@@ -48,10 +51,17 @@ def redirect(request):
 @app.route('/client-area')
 def clientArea(request):
     token = request.args.get(b"token", [None])[0]
-    print(token)
     if (token is None or token == b""):
         return "Error: Invalid/Empty Token"
     return ClientAreaElement()
+
+@app.route('/css/', branch=True)
+def css(request):
+    return File("./css")
+
+@app.route('/js/', branch=True)
+def js(request):
+    return File("./js")
 
 def onError(failure):
     print("Message Error: \n", failure)
@@ -70,7 +80,183 @@ def disconnected(client, reason):
 def onMessageReceived(client, message):
     print("Client Received a Message: \n", message)        
 
-hostType = input("Host (Live/Demo): ")
+authorizedAccounts = []
+
+def setAccount(accountId):
+    if accountId in authorizedAccounts:
+        sendProtoOAAccountLogoutReq(accountId)
+    sendProtoOAAccountAuthReq(accountId)
+
+def sendProtoOAVersionReq(clientMsgId = None):
+    request = ProtoOAVersionReq()
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAGetAccountListByAccessTokenReq(accessToken, clientMsgId = None):
+    request = ProtoOAGetAccountListByAccessTokenReq()
+    request.accessToken = accessToken
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAAccountLogoutReq(accountId, clientMsgId = None):
+    request = ProtoOAAccountLogoutReq()
+    request.ctidTraderAccountId = accountId
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAAccountAuthReq(accountId, accessToken, clientMsgId = None):
+    request = ProtoOAAccountAuthReq()
+    request.ctidTraderAccountId = accountId
+    request.accessToken = accessToken
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAAssetListReq(accountId, clientMsgId = None):
+    request = ProtoOAAssetListReq()
+    request.ctidTraderAccountId = accountId
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAAssetClassListReq(accountId, clientMsgId = None):
+    request = ProtoOAAssetClassListReq()
+    request.ctidTraderAccountId = accountId
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOASymbolCategoryListReq(accountId, clientMsgId = None):
+    request = ProtoOASymbolCategoryListReq()
+    request.ctidTraderAccountId = accountId
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOASymbolsListReq(accountId, includeArchivedSymbols = False, clientMsgId = None):
+    request = ProtoOASymbolsListReq()
+    request.ctidTraderAccountId = accountId
+    request.includeArchivedSymbols = includeArchivedSymbols if type(includeArchivedSymbols) is bool else bool(includeArchivedSymbols)
+    deferred = client.send(request)
+    deferred.addErrback(onError)
+
+def sendProtoOATraderReq(accountId, clientMsgId = None):
+    request = ProtoOATraderReq()
+    request.ctidTraderAccountId = accountId
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAUnsubscribeSpotsReq(accountId, symbolId, clientMsgId = None):
+    request = ProtoOAUnsubscribeSpotsReq()
+    request.ctidTraderAccountId = accountId
+    request.symbolId.append(int(symbolId))
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOASubscribeSpotsReq(accountId, symbolId, timeInSeconds, subscribeToSpotTimestamp	= False, clientMsgId = None):
+    request = ProtoOASubscribeSpotsReq()
+    request.ctidTraderAccountId = accountId
+    request.symbolId.append(int(symbolId))
+    request.subscribeToSpotTimestamp = subscribeToSpotTimestamp if type(subscribeToSpotTimestamp) is bool else bool(subscribeToSpotTimestamp)
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+    reactor.callLater(int(timeInSeconds), sendProtoOAUnsubscribeSpotsReq, symbolId)
+
+def sendProtoOAReconcileReq(accountId, clientMsgId = None):
+    request = ProtoOAReconcileReq()
+    request.ctidTraderAccountId = accountId
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAGetTrendbarsReq(accountId, weeks, period, symbolId, clientMsgId = None):
+    request = ProtoOAGetTrendbarsReq()
+    request.ctidTraderAccountId = accountId
+    request.period = ProtoOATrendbarPeriod.Value(period)
+    request.fromTimestamp = int((datetime.datetime.utcnow() - datetime.timedelta(weeks=int(weeks))).timestamp()) * 1000
+    request.toTimestamp = int(datetime.datetime.utcnow().timestamp()) * 1000
+    request.symbolId = int(symbolId)
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOAGetTickDataReq(accountId, days, quoteType, symbolId, clientMsgId = None):
+    request = ProtoOAGetTickDataReq()
+    request.ctidTraderAccountId = accountId
+    request.type = ProtoOAQuoteType.Value(quoteType.upper())
+    request.fromTimestamp = int((datetime.datetime.utcnow() - datetime.timedelta(days=int(days))).timestamp()) * 1000
+    request.toTimestamp = int(datetime.datetime.utcnow().timestamp()) * 1000
+    request.symbolId = int(symbolId)
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOANewOrderReq(accountId, symbolId, orderType, tradeSide, volume, price = None, clientMsgId = None):
+    request = ProtoOANewOrderReq()
+    request.ctidTraderAccountId = accountId
+    request.symbolId = int(symbolId)
+    request.orderType = ProtoOAOrderType.Value(orderType.upper())
+    request.tradeSide = ProtoOATradeSide.Value(tradeSide.upper())
+    request.volume = int(volume) * 100
+    if request.orderType == ProtoOAOrderType.LIMIT:
+        request.limitPrice = float(price)
+    elif request.orderType == ProtoOAOrderType.STOP:
+        request.stopPrice = float(price)
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendNewMarketOrder(symbolId, tradeSide, volume, clientMsgId = None):
+    sendProtoOANewOrderReq(symbolId, "MARKET", tradeSide, volume, clientMsgId = clientMsgId)
+
+def sendNewLimitOrder(symbolId, tradeSide, volume, price, clientMsgId = None):
+    sendProtoOANewOrderReq(symbolId, "LIMIT", tradeSide, volume, price, clientMsgId)
+
+def sendNewStopOrder(symbolId, tradeSide, volume, price, clientMsgId = None):
+    sendProtoOANewOrderReq(symbolId, "STOP", tradeSide, volume, price, clientMsgId)
+
+def sendProtoOAClosePositionReq(accountId, positionId, volume, clientMsgId = None):
+    request = ProtoOAClosePositionReq()
+    request.ctidTraderAccountId = accountId
+    request.positionId = int(positionId)
+    request.volume = int(volume) * 100
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+def sendProtoOACancelOrderReq(accountId, orderId, clientMsgId = None):
+    request = ProtoOACancelOrderReq()
+    request.ctidTraderAccountId = accountId
+    request.orderId = int(orderId)
+    deferred = client.send(request, clientMsgId = clientMsgId)
+    deferred.addErrback(onError)
+
+commands = {
+        "setAccount": setAccount,
+        "ProtoOAVersionReq": sendProtoOAVersionReq,
+        "ProtoOAGetAccountListByAccessTokenReq": sendProtoOAGetAccountListByAccessTokenReq,
+        "ProtoOAAssetListReq": sendProtoOAAssetListReq,
+        "ProtoOAAssetClassListReq": sendProtoOAAssetClassListReq,
+        "ProtoOASymbolCategoryListReq": sendProtoOASymbolCategoryListReq,
+        "ProtoOASymbolsListReq": sendProtoOASymbolsListReq,
+        "ProtoOATraderReq": sendProtoOATraderReq,
+        "ProtoOASubscribeSpotsReq": sendProtoOASubscribeSpotsReq,
+        "ProtoOAReconcileReq": sendProtoOAReconcileReq,
+        "ProtoOAGetTrendbarsReq": sendProtoOAGetTrendbarsReq,
+        "ProtoOAGetTickDataReq": sendProtoOAGetTickDataReq,
+        "NewMarketOrder": sendNewMarketOrder,
+        "NewLimitOrder": sendNewLimitOrder,
+        "NewStopOrder": sendNewStopOrder,
+        "ClosePosition": sendProtoOAClosePositionReq,
+        "CancelOrder": sendProtoOACancelOrderReq}
+
+@app.route('/get-data')
+def getData(request):
+    request.responseHeaders.addRawHeader(b"content-type", b"application/json")
+    token = request.args.get(b"token", [None])[0]
+    result = ""
+    if (token is None or token == b""):
+        result = "Invalid Token"
+    command = request.args.get(b"command", [None])[0]
+    if (command is None or command == b"" or command not in commands):
+        result = "Invalid Command"
+    else:
+        result = commands[command]()
+    return f'{{"result": "{result}"}}'.encode(encoding = 'UTF-8')
+
+#hostType = input("Host (Live/Demo): ")
+hostType = "demo"
 hostType = hostType.lower()
 
 while hostType != "live" and  hostType != "demo":
